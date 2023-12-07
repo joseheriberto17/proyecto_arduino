194e7298c149caf28b17bb98639a7f095a4084fc
theheri24 <jose.heriberto@hotmail.es>
2023-12-07 10:30:32 -0500

    inicio del proyecto Protoboard para pruebas

---
+++ proyecto_arduino.ino
@@ -0,0 +1,153 @@
+/*
+  Nombre del Archivo: proyecto_arduino.ino
+  Descripción: programa que recopila las capacidades mas intersante del arduino uno.
+    secuencia de led de tre colores
+
+  Autor: jose heriberto marquez diaz
+  Fecha: 5 de diciembre de 2024
+
+  Notas:
+  - Arduino CLI, librerias por definir
+*/
+
+// ------------------------------------
+// Inclusión de Librerías
+// ------------------------------------
+#include <stdio.h>
+// ------------------------------------
+// Definición de Constantes
+// ------------------------------------
+#define PIN_LED 13
+#define PIN_LED_1 3
+#define PIN_LED_2 5
+#define PIN_LED_3 6
+#define PIN_LED_4 9
+
+
+#define VELOCIDAD_SERIAL 9600
+
+// ------------------------------------
+// Variables Globales
+// ------------------------------------
+
+// secuencia led
+uint8_t counter_led = 0;
+uint8_t dir_led = 0; 
+uint8_t size_led = 4;
+
+unsigned long sample_time = 500;
+
+uint16_t gpio_pins[4] = {PIN_LED_1, PIN_LED_2, PIN_LED_3,PIN_LED_4};
+const uint8_t M_LED[4][4] = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+
+// ------------------------------------
+// Configuración Inicial
+// ------------------------------------
+void setup()
+{
+  Serial.begin(VELOCIDAD_SERIAL);
+
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_LED_1, OUTPUT);
+  pinMode(PIN_LED_2, OUTPUT);
+  pinMode(PIN_LED_3, OUTPUT);
+  pinMode(PIN_LED_4, OUTPUT);
+  
+
+  digitalWrite(PIN_LED_1, LOW); // Enciende el LED
+  digitalWrite(PIN_LED_2, LOW); // Enciende el LED
+  digitalWrite(PIN_LED_3, LOW); // Enciende el LED
+  digitalWrite(PIN_LED_4, LOW); // Enciende el LED
+}
+
+// ------------------------------------
+// Función Principal
+// ------------------------------------
+void loop()
+{
+  // Código principal del programa
+  if (counter_led == 4)
+  {
+    counter_led = 0;
+  }
+  for (int i = 0; i < 4; ++i)
+  {
+    digitalWrite(gpio_pins[i], M_LED[counter_led][(dir_led==0)? (i):(-i + size_led-1)]);
+
+  }
+  counter_led++;
+  delay(sample_time);
+}
+
+// ------------------------------------
+// Funciónes secundarias
+// ------------------------------------
+void Toogle_led(uint8_t pin)
+{
+  if (digitalRead(pin) == 0)
+  {
+    digitalWrite(pin, 1);
+  }
+  else
+  {
+    digitalWrite(pin, 0);
+  }
+}
+
+uint8_t toogle_data(uint8_t variable){
+	if (variable == 0) {
+		variable = 1;
+	} else {
+		variable = 0;
+	}
+	return variable;
+}
+// void imprimir_intro()
+// {
+//   println("bienvenido")
+// }
+
+
+void serialEvent() {
+  while(Serial.available())
+  {
+    String dataString = Serial.readStringUntil('\n');
+
+    Serial.println("comando  recibido: " + dataString);
+
+    // char datas =(char) dataString.c_str();
+
+    // Serial.println(datas);
+    
+    char letra;
+    int numero;
+    char data[10];
+    dataString.toCharArray(data,dataString.length()+1);
+
+    int datas = sscanf(data,"%c%d",&letra,&numero);
+
+    Serial.print("letra: ");
+    Serial.println(letra);
+    Serial.print("numero: ");
+    Serial.println(numero);
+
+    switch (letra)
+    {
+    case 'w':
+      dir_led = 0;
+      Serial.println("direccion: adelante");
+      break;
+    case 's':
+      dir_led = 1;
+      Serial.println("direccion: atras");
+      break;
+    case 't':
+      sample_time = numero;
+      break;
+    
+    default:
+      break;
+    }
+  }
+}
+

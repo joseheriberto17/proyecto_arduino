0e7a400e087c9f9c3bc2bba8f0c15f13c6d890c8
theheri24 <jose.heriberto@hotmail.es>
2023-12-10 00:32:38 -0500

    1) reconfiguracion de los pines led y botones 2) interupcciones de los pines 2 (toogle_dir) y 3(toogle_stop)

--- proyecto_arduino.ino
+++ proyecto_arduino.ino
@@ -18,13 +18,13 @@
 // Definición de Constantes
 // ------------------------------------
 #define PIN_LED 13
-#define PIN_LED_1 3
-#define PIN_LED_2 5
-#define PIN_LED_3 6
-#define PIN_LED_4 9
-#define PIN_LED_5 10
-#define PIN_BUTTON_1 4
-#define PIN_BUTTON_1 7
+#define PIN_LED_1 5
+#define PIN_LED_2 6
+#define PIN_LED_3 9
+#define PIN_LED_4 10
+#define PIN_LED_5 11
+#define PIN_BUTTON_1 2
+#define PIN_BUTTON_2 3
 
 
 #define VELOCIDAD_SERIAL 9600
@@ -48,8 +48,17 @@ unsigned long sample_time = 500;
 uint16_t gpio_pins[4] = {PIN_LED_1, PIN_LED_2, PIN_LED_3,PIN_LED_4};
 const uint8_t M_LED[4][4] = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
 
+//interrupcciones
+// pin 3
+void toogle_dir(){
+  toogle_data(dir_led);
+  delay(100);
+}
 
-
+void toogle_stop(){
+  toogle_data(stop);
+  delay(100);
+}
 // ------------------------------------
 // Configuración Inicial
 // ------------------------------------
@@ -64,12 +73,16 @@ void setup()
   pinMode(PIN_LED_4, OUTPUT);
   pinMode(PIN_LED_5, OUTPUT);
   
+  pinMode(PIN_BUTTON_1,INPUT_PULLUP);
+  pinMode(PIN_BUTTON_2,INPUT_PULLUP);
 
   digitalWrite(PIN_LED_1, LOW); // Enciende el LED
   digitalWrite(PIN_LED_2, LOW); // Enciende el LED
   digitalWrite(PIN_LED_3, LOW); // Enciende el LED
   digitalWrite(PIN_LED_4, LOW); // Enciende el LED
 
+  attachInterrupt(digitalPinToInterrupt(2),toogle_dir,RISING);
+  attachInterrupt(digitalPinToInterrupt(3),toogle_stop,RISING);
 }
 
 // ------------------------------------
@@ -81,7 +94,8 @@ void loop()
   
   if (stop == 0)
   {
-    counter_led++;    
+    counter_led++;   
+  } 
     if (counter_led == 4)
     {
       counter_led = 0;
@@ -93,7 +107,7 @@ void loop()
     }
     analogWrite(PIN_LED_5,power_led);
     delay(sample_time);
-  }
+  
 }
 
 // ------------------------------------
@@ -111,18 +125,13 @@ void Toogle_led(uint8_t pin)
   }
 }
 
-void toogle_data(bool variable){
-	if (variable == true) {
-		variable = false;
+void toogle_data(uint8_t &variable){
+	if (variable == 1) {
+		variable = 0;
 	} else {
-		variable = true;
+		variable = 1;
 	}
 }
-
-// void imprimir_intro()
-// {
-//   println("bienvenido")
-// }
 
 void serialEvent() {
   if (Serial.available() > 0)
@@ -175,10 +184,9 @@ void serialEvent() {
         stop = 1;
       }
       Serial.print("stop: ");
-      Serial.println(stop);
-      
+      Serial.println(stop);      
       break;
-    
+
     default:
       break;
     }
